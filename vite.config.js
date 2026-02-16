f2a8b9204b9dcd8beea3cc1534866fad80c120eb
abhiramsuresh500 <abhiramsuresh463@gmail.com>
2026-02-16 12:07:53 +0530

    fix: update .gitignore to include /dist and modify base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  base: "/miniproject/",   // 👈 IMPORTANT FOR GITHUB PAGES
+  base: "/", 
   plugins: [react()],
   resolve: {
     alias: {
